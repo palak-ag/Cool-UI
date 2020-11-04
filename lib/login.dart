211c6d458c29fd548b05affbf6b89cc852a0d471
Gnaneshdivi <gnanesh_divi@srmap.edu.in>
2020-11-04 09:30:59 +0500

    hello

---
+++ lib/login.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dashboard_screen.dart';
+import 'login_screen.dart';
+import 'transition_route_observer.dart';
+
+class Loginpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      theme: ThemeData(
+        // brightness: Brightness.dark,
+
+        accentColor: Colors.orange,
+        cursorColor: Colors.orange,
+        // fontFamily: 'SourceSansPro',
+        textTheme: TextTheme(
+          display2: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            // fontWeight: FontWeight.w400,
+            color: Colors.orange,
+          ),
+          button: TextStyle(
+            // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+            fontFamily: 'OpenSans',
+          ),
+          caption: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.deepPurple[300],
+          ),
+          overline: TextStyle(fontFamily: 'NotoSans'),
+        ),
+      ),
+      home: LoginScreen(),
+      navigatorObservers: [TransitionRouteObserver()],
+      routes: {
+        LoginScreen.routeName: (context) => LoginScreen(),
+        DashboardScreen.routeName: (context) => DashboardScreen(),
+      },
+    );
+  }
+}
---
+++ lib/users.dart
@@ -0,0 +1,9 @@
+const mockUsers = const {
+  'divignanesh@gmail.com': 'medica',
+  'g.lakshmisb@gmail.com': 'medica',
+  'arpith@gmail.com': 'medica',
+  'naganitinsydney@gmail.com': 'medica',
+  'pulkit@gmail.com': 'medica',
+  '@.com': '.',
+  '123@gmail.com': '1234',
+};
